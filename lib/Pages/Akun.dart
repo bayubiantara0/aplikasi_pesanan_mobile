78961dfc9587df301ed20fe521975f085a69903d
Tuntaz Dev 143 <maulibrahim20@student.polindra.ac.id>
2024-11-22 15:04:06 +0700

    update

--- lib/Pages/Akun.dart
+++ lib/Pages/Akun.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:wkwk/User_model.dart';
 import 'package:wkwk/header.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Akun extends StatelessWidget {
   const Akun({Key? key}) : super(key: key);
@@ -11,21 +11,11 @@ class Akun extends StatelessWidget {
   final String phoneNumber = "(0234) 5746464";
   final String address =
       "Jl. Lohbener Lama No.08, Legok, Kec. Lohbener, Kabupaten Indramayu, Jawa Barat 45252";
-  final double latitude = -6.40816; // Ganti dengan latitude lokasi Anda
-  final double longitude = 106.28144; // Ganti dengan longitude lokasi Anda
 
   Future<void> _logOut(BuildContext context) async {
     await UserModel.logOut();
     Navigator.of(context)
         .pushNamedAndRemoveUntil('/login', (Route<dynamic> route) => false);
-  }
-
-  void _launchMaps() async {
-    final url =
-        'https://www.google.com/maps/search/?api=1&query=$latitude,$longitude';
-    if (await canLaunch(url)) {
-      await launch(url);
-    }
   }
 
   void _launchPhone() async {
@@ -108,7 +98,7 @@ class Akun extends StatelessWidget {
                           ListTile(
                             leading: Icon(Icons.business, color: Colors.blue),
                             title: Text(companyName),
-                            subtitle: Text('Nama Perusahaan'),
+                            subtitle: Text('Nama Kampus'),
                           ),
                           ListTile(
                             leading: Icon(Icons.phone, color: Colors.green),
@@ -120,7 +110,6 @@ class Akun extends StatelessWidget {
                             leading: Icon(Icons.location_on, color: Colors.red),
                             title: Text(address),
                             subtitle: Text('Alamat'),
-                            onTap: _launchMaps,
                           ),
                         ],
                       ),
@@ -132,29 +121,43 @@ class Akun extends StatelessWidget {
                     child: Column(
                       children: [
                         Container(
-                          height: 200,
-                          child: GoogleMap(
-                            initialCameraPosition: CameraPosition(
-                              target: LatLng(latitude, longitude),
-                              zoom: 15,
-                            ),
-                            markers: {
-                              Marker(
-                                markerId: MarkerId('company_location'),
-                                position: LatLng(latitude, longitude),
-                                infoWindow: InfoWindow(
-                                  title: companyName,
-                                  snippet: address,
-                                ),
+                          height: 250,
+                          child: InAppWebView(
+                            initialUrlRequest: URLRequest(
+                              url: Uri.parse(
+                                'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.888132250622!2d108.2788767758701!3d-6.408409362676406!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6eb87d1fcaf97d%3A0x4fc15b3c8407ada4!2sPoliteknik%20Negeri%20Indramayu!5e0!3m2!1sid!2sid!4v1732261960329!5m2!1sid!2sid',
                               ),
+                            ),
+                            onLoadError: (controller, url, code, message) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text("Error loading map!")),
+                              );
                             },
+                            initialOptions: InAppWebViewGroupOptions(
+                              crossPlatform: InAppWebViewOptions(
+                                javaScriptEnabled: true,
+                                supportZoom: false,
+                              ),
+                            ),
                           ),
                         ),
                         Padding(
                           padding: const EdgeInsets.all(8.0),
                           child: ElevatedButton.icon(
                             icon: Icon(Icons.map),
-                            onPressed: _launchMaps,
+                            onPressed: () async {
+                              const url =
+                                  'https://www.google.com/maps/search/?api=1&query=-6.408409362676406,108.2788767758701';
+                              if (await canLaunch(url)) {
+                                await launch(url);
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Text("Could not launch maps"),
+                                  ),
+                                );
+                              }
+                            },
                             label: Text('Buka di Google Maps'),
                             style: ElevatedButton.styleFrom(
                               padding: EdgeInsets.symmetric(
